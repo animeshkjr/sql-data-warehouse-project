4d81febe4906ce5fd535baff229d51dd7c161b11
Animesh Kujur <animeshkjr1@gmail.com>
2025-05-17 02:28:00 +0530

    Update Analysis 4: Part to Whole Proportional Analysis(Method 2).sql

--- scripts/4. Data Analytics/B. Advance Data Analytics/Analysis 4: Part to Whole Proportional Analysis(Method 2).sql
+++ scripts/4. Data Analytics/B. Advance Data Analytics/Analysis 4: Part to Whole Proportional Analysis(Method 2).sql
@@ -10,3 +10,15 @@ Highlights:
     2. Determines each category’s proportion of overall sales.
     3. Helps identify dominant and underperforming categories in the sales mix.
 ===========================================================================
+SELECT
+    b.category,
+    SUM(a.sales)              AS category_total_sales,
+    SUM(SUM(a.sales)) OVER () AS overall_total_sales,
+    CONCAT(ROUND((CAST(SUM(a.sales) AS FLOAT) / SUM(SUM(a.sales)) OVER ()) * 100, 2), '%') AS sales_percentage
+FROM 
+    gold.fact_sales   AS a
+LEFT JOIN 
+    gold.dim_products AS b
+  ON a.product_key = b.product_key
+GROUP BY b.category
+ORDER BY category_total_sales DESC;
