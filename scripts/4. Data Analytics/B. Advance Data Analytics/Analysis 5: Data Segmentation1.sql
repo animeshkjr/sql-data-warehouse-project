26f5b2b7da5d002a53ec09cd9f8d781ca46ec067
Animesh Kujur <animeshkjr1@gmail.com>
2025-05-17 02:33:07 +0530

    Update Analysis 5: Data Segmentation1.sql

--- scripts/4. Data Analytics/B. Advance Data Analytics/Analysis 5: Data Segmentation1.sql
+++ scripts/4. Data Analytics/B. Advance Data Analytics/Analysis 5: Data Segmentation1.sql
@@ -17,10 +17,10 @@ WITH product_segments AS
 	product_key,
 	product_name,
 	product_cost,
-	CASE WHEN product_cost < 100 THEN 'Below 100'
-		   WHEN product_cost BETWEEN 100 AND 500 THEN '100 - 500'	
-		   WHEN product_cost BETWEEN 500 AND 1000 THEN '500 - 1000'
-		   ELSE 'Above 1000'
+	CASE 	WHEN product_cost < 100 THEN 'Below 100'
+		WHEN product_cost BETWEEN 100 AND 500 THEN '100 - 500'	
+		WHEN product_cost BETWEEN 500 AND 1000 THEN '500 - 1000'
+		ELSE 'Above 1000'
 	END AS cost_range
 FROM 
 	gold.dim_products)
