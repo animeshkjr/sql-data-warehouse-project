ac8754b3e6b4ffb8b824eb0a759ec13dad9209d2
Animesh Kujur <animeshkjr1@gmail.com>
2025-05-17 02:26:27 +0530

    Create Analysis 4: Part to Whole Proportional Analysis(Method 2).sql

---
+++ scripts/4. Data Analytics/B. Advance Data Analytics/Analysis 4: Part to Whole Proportional Analysis(Method 2).sql
@@ -0,0 +1,12 @@
+/*
+===========================================================================
+ANALYSIS 4: PART TO WHOLE PROPORTIONAL ANALYSIS
+===========================================================================
+Purpose:
+    - To understand how different categories contribute to total sales.
+
+Highlights:
+    1. Calculates total sales by category.
+    2. Determines each category’s proportion of overall sales.
+    3. Helps identify dominant and underperforming categories in the sales mix.
+===========================================================================
