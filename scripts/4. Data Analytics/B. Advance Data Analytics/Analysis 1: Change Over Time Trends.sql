a79b15ed7914e87ec583af99ec18643bd9ceabf3
Animesh Kujur <animeshkjr1@gmail.com>
2025-05-17 02:07:45 +0530

    Update Analysis 1: Change Over Time Trends.sql

--- scripts/4. Data Analytics/B. Advance Data Analytics/Analysis 1: Change Over Time Trends.sql
+++ scripts/4. Data Analytics/B. Advance Data Analytics/Analysis 1: Change Over Time Trends.sql
@@ -1,1 +1,25 @@
+/*
+===========================================================================
+ANALYSIS 1: CHANGE OVER TIME TRENDS
+===========================================================================
+Purpose:
+    - To analyze year-over-year trends in sales performance and customer activity.
 
+Highlights:
+    1. Calculates annual totals for:
+        - Sales revenue
+        - Unique customers
+        - Quantity sold
+    2. Enables time-series insights into business growth and customer engagement.
+===========================================================================*/
+SELECT
+  	YEAR (sales_order_date)				AS order_year,
+  	SUM	 (sales)						      AS total_sales,
+  	COUNT (DISTINCT customer_id)	AS customer_count,
+  	SUM	 (quantity)						    AS total_unit_sale
+FROM 
+	  gold.fact_sales
+WHERE 
+	  sales_order_date IS NOT NULL
+GROUP BY YEAR (sales_order_date)
+ORDER BY YEAR (sales_order_date);
