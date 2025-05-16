417dbdaf0d09cbab09b6b8a1ce99b9aab46db3c3
Animesh Kujur <animeshkjr1@gmail.com>
2025-05-17 02:32:03 +0530

    Update Analysis 5: Data Segmentation1.sql

--- scripts/4. Data Analytics/B. Advance Data Analytics/Analysis 5: Data Segmentation1.sql
+++ scripts/4. Data Analytics/B. Advance Data Analytics/Analysis 5: Data Segmentation1.sql
@@ -1,1 +1,35 @@
+/*
+===========================================================================
+ANALYSIS 5: DATA SEGMENTATION
+===========================================================================
+Purpose:
+    - To categorize products into cost-based segments for better analysis.
 
+Highlights:
+    1. Segments products into defined cost ranges.
+    2. Counts the number of products within each segment.
+    3. Useful for pricing strategy, inventory planning, and product mix analysis.
+===========================================================================
+*/
+
+WITH product_segments AS
+(SELECT 
+	product_key,
+	product_name,
+	product_cost,
+	CASE WHEN product_cost < 100 THEN 'Below 100'
+		   WHEN product_cost BETWEEN 100 AND 500 THEN '100 - 500'	
+		   WHEN product_cost BETWEEN 500 AND 1000 THEN '500 - 1000'
+		   ELSE 'Above 1000'
+	END AS cost_range
+FROM gold.dim_products)
+
+SELECT
+	cost_range,
+	COUNT(product_key) AS total_products
+FROM 
+	product_segments
+GROUP BY 
+	cost_range
+ORDER BY
+	total_products DESC;
