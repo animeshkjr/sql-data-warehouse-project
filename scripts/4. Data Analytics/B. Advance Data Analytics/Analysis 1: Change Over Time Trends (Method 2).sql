3dc3dfe339a36801e8dda5342845643b253cabc3
Animesh Kujur <animeshkjr1@gmail.com>
2025-05-17 02:08:47 +0530

    Update Analysis 1: Change Over Time Trends (Method 2).sql

--- scripts/4. Data Analytics/B. Advance Data Analytics/Analysis 1: Change Over Time Trends (Method 2).sql
+++ scripts/4. Data Analytics/B. Advance Data Analytics/Analysis 1: Change Over Time Trends (Method 2).sql
@@ -1,1 +1,25 @@
+/*
+===========================================================================
+ANALYSIS 1: CHANGE OVER TIME TRENDS
+===========================================================================
+Purpose:
+    - To analyze year-over-year trends in sales performance and customer activity.
 
+Highlights:
+    1. Calculates annual totals for (Method 2):
+        - Sales revenue
+        - Unique customers
+        - Quantity sold
+    2. Enables time-series insights into business growth and customer engagement.
+===========================================================================*/
+SELECT
+  	DATETRUNC (year, sales_order_date)  AS order_year,
+  	SUM	(sales)								          AS total_sales,
+  	COUNT (DISTINCT customer_id)			  AS customer_count,
+  	SUM	(quantity)							        AS total_unit_sale
+FROM 
+	  gold.fact_sales
+WHERE 
+	  sales_order_date IS NOT NULL
+GROUP BY DATETRUNC	(year, sales_order_date)
+ORDER BY DATETRUNC	(year, sales_order_date);
